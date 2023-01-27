f4ebd162dce5348a2cee6b048ee0f6144df46e12
akula993 <akula993@gmail.com>
2023-01-27 08:35:42 +0300

    Создать датчики и выставить их в шаблоне

--- vend/models.py
+++ vend/models.py
@@ -9,6 +9,7 @@ class Address(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True, null=True,)
     uuid = models.UUIDField( blank=True, null=True,)
+
     created_at = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     to_rent = models.DecimalField('Аренда', max_digits=10, decimal_places=2, blank=True, null=True, )
